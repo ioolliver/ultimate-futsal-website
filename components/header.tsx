83da325d5fb4567604b839b7a877051ba9f398bb
ioolliver <a>
2024-04-21 21:31:21 -0300

    Initial commit

---
+++ components/header.tsx
@@ -0,0 +1,19 @@
+import { User } from "lucide-react";
+import Image from "next/image";
+import Link from "next/link";
+
+export function Header() {
+    return (
+        <header className="px-12 py-4 flex justify-between items-center">
+            <div className="">
+                <Image src="/logo.png" alt="Ultimate Futsal" width={50} height={50} />
+            </div>
+            <Link href="/">
+                <div className="bg-uf-secondary-2 px-4 py-2 rounded-full flex gap-4">
+                    <User color="#df6311" size={24} />
+                    <p>Play now</p>
+                </div>
+            </Link>
+        </header>
+    )
+}
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,6 +8,15 @@ const config: Config = {
   ],
   theme: {
     extend: {
+      colors: {
+        "uf-background": "#05061f",
+        "uf-primary": "#FF8706",
+        "uf-secondary": "#1f308d",
+        "uf-secondary-2": "#3F4D9B",
+        "uf-secondary-3": "#657FFF",
+        "uf-accent": "#FF784B",
+        "uf-accent-2": "#FFA106"
+      },
       backgroundImage: {
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
         "gradient-conic":
